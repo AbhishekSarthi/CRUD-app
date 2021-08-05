25b5922bbb65bc56792c2a503bfda2a543e50137
Abhishek Sarthi <abhi.sarthi15@gmail.com>
2021-08-05 13:14:48 +0530

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const crudRoutes = require('./routes/crud-routes');
-// const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cors());
 
 // app.use(
 //     cors({
